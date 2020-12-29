760328f702a43b4e8318744f3257c155b74207b0
Rishu Shrivastava <rishu.shrivastava@gmail.com>
2020-12-29 18:27:49 +0000

    game 1 complete

--- ShootingGallery/main.lua
+++ ShootingGallery/main.lua
@@ -11,12 +11,29 @@ function love.load()
 
     score=0
     timer=0
+    gameState=1
 
     gameFont = love.graphics.newFont(40)
+
+    sprites = {}
+    sprites.sky = love.graphics.newImage('sprites/sky.png')
+    sprites.target = love.graphics.newImage('sprites/target.png')
+    sprites.crosshairs = love.graphics.newImage('sprites/crosshairs.png')
+
+    love.mouse.setVisible(false)
+
 end
 
 function love.update(dt)
     -- number = number + 1
+    if timer > 0 then
+        timer = timer - dt
+    end
+    
+    if timer < 0 then
+        timer = 0
+        gameState = 1
+    end
 end
 
 function love.draw()
@@ -27,19 +44,34 @@ function love.draw()
     -- love.graphics.setColor(204/255, 51/255, 255/255)
     --love.graphics.circle("fill",300,200,100,100)
 
+    -- draw the background
+    love.graphics.draw(sprites.sky,0,0)
+
     -- drawing the circle
-    love.graphics.setColor(1,0,0)
-    love.graphics.circle("fill",target.x,target.y, target.radius)
+    -- love.graphics.setColor(1,0,0)
+    -- love.graphics.circle("fill",target.x,target.y, target.radius)
 
     -- printing the score
     love.graphics.setColor(1,1,1) -- white
     love.graphics.setFont(gameFont)
-    love.graphics.print(score, 0, 0) 
+    love.graphics.print("Score: "..score, 5, 5) 
+    love.graphics.print("Timer: "..math.ceil(timer), 300, 5)
+
+    if gameState == 1 then
+        love.graphics.printf("Click anywhere to begin !!!",0,250,love.graphics.getWidth(),"center")
+    end
+
+    if gameState == 2 then
+        love.graphics.draw(sprites.target, target.x - target.radius, target.y - target.radius)
+    end
+
+    love.graphics.draw(sprites.crosshairs, love.mouse.getX()-20, love.mouse.getY()-20)
+
 end
 
 -- for handling mouse press by the user.
 function love.mousepressed(x, y, button, istouch, presses)
-    if button == 1 then
+    if button == 1 and gameState == 2 then
         local mouseToPressed = distanceBetween(x,y, target.x, target.y)
         if mouseToPressed < target.radius then
             score = score + 1
@@ -47,8 +79,14 @@ function love.mousepressed(x, y, button, istouch, presses)
             -- put some randomnees to the code
             target.x = math.random(target.radius, love.graphics.getWidth() - target.radius)
             target.y = math.random(target.radius, love.graphics.getHeight() - target.radius)
-        end        
-    end
+
+        end  
+    elseif button == 1 and gameState ==1 then
+        gameState = 2
+        timer = 10
+        score = 0
+    end      
+    
 end
 
 -- calculate the distance
